7e49f71e98c99aa347f3369fb7ddd2102ff54c81
pengyao <pengyao_fo@163.com>
2014-10-11 16:28:58 +0800

    new test

---
+++ js/product.js
@@ -0,0 +1,119 @@
+	function windowOnload() {
+		document.getElementById("xihuProCont").style.display = 'block';
+		document.getElementById("huliFaceCont").style.display = 'none';
+		document.getElementById("huliHandCont").style.display = 'none';
+		document.getElementById("huliBodyCont").style.display = 'none';
+		document.getElementById("otherProCont").style.display = 'none';
+		
+		document.getElementById("xihuPro").style.backgroundImage = 'url(img/menu_bar_bg.png)';
+		document.getElementById("huliFace").style.backgroundImage = 'none';
+		document.getElementById("huliHand").style.backgroundImage = 'none';
+		document.getElementById("huliBody").style.backgroundImage = 'none';
+		document.getElementById("otherPro").style.backgroundImage = 'none';
+		
+		document.getElementById("xihuPro").style.color = '#f60100';
+		document.getElementById("huliFace").style.color = 'rgb(255, 255, 255)';
+		document.getElementById("huliHand").style.color = 'rgb(255, 255, 255)';
+		document.getElementById("huliBody").style.color = 'rgb(255, 255, 255)';
+		document.getElementById("otherPro").style.color = 'rgb(255, 255, 255)';
+	}
+	
+	function checkXihu() {
+		document.getElementById("xihuProCont").style.display = 'block';
+		document.getElementById("huliFaceCont").style.display = 'none';
+		document.getElementById("huliHandCont").style.display = 'none';
+		document.getElementById("huliBodyCont").style.display = 'none';
+		document.getElementById("otherProCont").style.display = 'none';
+		
+		document.getElementById("xihuPro").style.backgroundImage = 'url(img/menu_bar_bg.png)';
+		document.getElementById("huliFace").style.backgroundImage = 'none';
+		document.getElementById("huliHand").style.backgroundImage = 'none';
+		document.getElementById("huliBody").style.backgroundImage = 'none';
+		document.getElementById("otherPro").style.backgroundImage = 'none';
+		
+		document.getElementById("xihuPro").style.color = '#f60100';
+		document.getElementById("huliFace").style.color = 'rgb(255, 255, 255)';
+		document.getElementById("huliHand").style.color = 'rgb(255, 255, 255)';
+		document.getElementById("huliBody").style.color = 'rgb(255, 255, 255)';
+		document.getElementById("otherPro").style.color = 'rgb(255, 255, 255)';
+	}
+	
+	function checkFace() {
+		document.getElementById("xihuProCont").style.display = 'none';
+		document.getElementById("huliFaceCont").style.display = 'block';
+		document.getElementById("huliHandCont").style.display = 'none';
+		document.getElementById("huliBodyCont").style.display = 'none';
+		document.getElementById("otherProCont").style.display = 'none';
+		
+		document.getElementById("xihuPro").style.backgroundImage = 'none';
+		document.getElementById("huliFace").style.backgroundImage = 'url(img/menu_bar_bg.png)';
+		document.getElementById("huliHand").style.backgroundImage = 'none';
+		document.getElementById("huliBody").style.backgroundImage = 'none';
+		document.getElementById("otherPro").style.backgroundImage = 'none';
+		
+		document.getElementById("xihuPro").style.color = 'rgb(255, 255, 255)';
+		document.getElementById("huliFace").style.color = '#f60100';
+		document.getElementById("huliHand").style.color = 'rgb(255, 255, 255)';
+		document.getElementById("huliBody").style.color = 'rgb(255, 255, 255)';
+		document.getElementById("otherPro").style.color = 'rgb(255, 255, 255)';
+	}
+	
+	function checkHand() {
+		document.getElementById("xihuProCont").style.display = 'none';
+		document.getElementById("huliFaceCont").style.display = 'none';
+		document.getElementById("huliHandCont").style.display = 'block';
+		document.getElementById("huliBodyCont").style.display = 'none';
+		document.getElementById("otherProCont").style.display = 'none';
+		
+		document.getElementById("xihuPro").style.backgroundImage = 'none';
+		document.getElementById("huliFace").style.backgroundImage = 'none';
+		document.getElementById("huliHand").style.backgroundImage = 'url(img/menu_bar_bg.png)';
+		document.getElementById("huliBody").style.backgroundImage = 'none';
+		document.getElementById("otherPro").style.backgroundImage = 'none';
+		
+		document.getElementById("xihuPro").style.color = 'rgb(255, 255, 255)';
+		document.getElementById("huliFace").style.color = 'rgb(255, 255, 255)';
+		document.getElementById("huliHand").style.color = '#f60100';
+		document.getElementById("huliBody").style.color = 'rgb(255, 255, 255)';
+		document.getElementById("otherPro").style.color = 'rgb(255, 255, 255)';
+	}
+	
+	function checkBody() {
+		document.getElementById("xihuProCont").style.display = 'none';
+		document.getElementById("huliFaceCont").style.display = 'none';
+		document.getElementById("huliHandCont").style.display = 'none';
+		document.getElementById("huliBodyCont").style.display = 'block';
+		document.getElementById("otherProCont").style.display = 'none';
+		
+		document.getElementById("xihuPro").style.backgroundImage = 'none';
+		document.getElementById("huliFace").style.backgroundImage = 'none';
+		document.getElementById("huliHand").style.backgroundImage = 'none';
+		document.getElementById("huliBody").style.backgroundImage = 'url(img/menu_bar_bg.png)';
+		document.getElementById("otherPro").style.backgroundImage = 'none';
+		
+		document.getElementById("xihuPro").style.color = 'rgb(255, 255, 255)';
+		document.getElementById("huliFace").style.color = 'rgb(255, 255, 255)';
+		document.getElementById("huliHand").style.color = 'rgb(255, 255, 255)';
+		document.getElementById("huliBody").style.color = '#f60100';
+		document.getElementById("otherPro").style.color = 'rgb(255, 255, 255)';
+	}
+	
+	function checkOther() {
+		document.getElementById("xihuProCont").style.display = 'none';
+		document.getElementById("huliFaceCont").style.display = 'none';
+		document.getElementById("huliHandCont").style.display = 'none';
+		document.getElementById("huliBodyCont").style.display = 'none';
+		document.getElementById("otherProCont").style.display = 'block';
+		
+		document.getElementById("xihuPro").style.backgroundImage = 'none';
+		document.getElementById("huliFace").style.backgroundImage = 'none';
+		document.getElementById("huliHand").style.backgroundImage = 'none';
+		document.getElementById("huliBody").style.backgroundImage = 'none';
+		document.getElementById("otherPro").style.backgroundImage = 'url(img/menu_bar_bg.png)';
+		
+		document.getElementById("xihuPro").style.color = 'rgb(255, 255, 255)';
+		document.getElementById("huliFace").style.color = 'rgb(255, 255, 255)';
+		document.getElementById("huliHand").style.color = 'rgb(255, 255, 255)';
+		document.getElementById("huliBody").style.color = 'rgb(255, 255, 255)';
+		document.getElementById("otherPro").style.color = '#f60100';
+	}
